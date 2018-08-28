0e754f8748777a98e5f091f5350861a02e0e855f
kadmiel01 <cadmielcamilo01@gmail.com>
2018-08-28 19:58:27 -0300

    atualiazando GERAL

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,15 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE) #chave primaria
-    titulo = models.CharField(max_length = 200) #limite do texto
-    texto = models.TextField()
-    created_date = models.DateField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey('auth.user', on_delete = models.CASCADE)
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    created_date = models.DateField (default = timezone.now)
+    published_date = models.DateTimeField (blank = True, null = True)
 
     def publish(self):
-     self.published_date = timezone.now()
-     self.save()
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
-        return self.titulo
+        return self.title
